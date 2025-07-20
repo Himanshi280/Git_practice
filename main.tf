1254bf3781af8846a54a7ad44ef8dc8edf9e957b
Himanshi <himanshi.soni@cloudeq.com>
2025-07-20 19:35:43 +0530

    Feature

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ module "eks_cluster" {
   depends_on = [ module.iam_role ]
 }
 
-module "node_group" {
-  source = "./modules/node-group"
-  node-group-details = var.node-group-details
-  node_role_arn = module.iam_role.node_group_arn
-  depends_on = [ module.eks_cluster, module.iam_role ]
-  # eks_version = module.eks_cluster.eks_version
-} 
+# module "node_group" {
+#   source = "./modules/node-group"
+#   node-group-details = var.node-group-details
+#   node_role_arn = module.iam_role.node_group_arn
+#   depends_on = [ module.eks_cluster, module.iam_role ]
+#   # eks_version = module.eks_cluster.eks_version
+# } 
