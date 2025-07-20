f795290fe4007449076d79b25905728ab259ce86
Himanshi <himanshi.soni@cloudeq.com>
2025-07-20 19:37:48 +0530

    changes

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ module "iam_role" {
   node_role_name = var.node_role_name
 }
 
-module "eks_cluster" {
-  source = "./modules/eks"
-  cluster-details = var.cluster-details
-  role_arn = module.iam_role.cluster_role_arn
-  depends_on = [ module.iam_role ]
-}
+# module "eks_cluster" {
+#   source = "./modules/eks"
+#   cluster-details = var.cluster-details
+#   role_arn = module.iam_role.cluster_role_arn
+#   depends_on = [ module.iam_role ]
+# }
 
 module "node_group" {
   source = "./modules/node-group"
